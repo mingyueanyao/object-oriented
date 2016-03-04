c2be88ecfcd985efb58a5739ce9909f5a993fce7
mingyueanyao <1981167302@qq.com>
2016-03-04 22:50:00 +0800

    code finish

--- 1001. A+B Format (20)/1001. A+B Format (20).cpp
+++ 1001. A+B Format (20)/1001. A+B Format (20).cpp
@@ -1,10 +1,14 @@
 #include<stdio.h>
+
 int main ()
 {
-  int a,b,c,d[7],i=0,j=0;
+  int a,b,c,d[7],i=0;
+  
   scanf("%d %d",&a,&b);
   c=a+b;
+  
   if(c==0) printf("%d",c);   
+  
   else
 	{
 	  if(c<0) 
@@ -12,18 +16,20 @@ int main ()
 	    c=-c;
 	    printf("-");
 	  }
+	  
 	  while(c>0)
 	   {
 		d[i]=c%10;
 		c=c/10;
 		i++;
-		j++;
 	   }	
 	}
-  for(;0<j;j--)
+	
+  for(;0<i;i--)
   {
-  	printf("%d",d[j-1]);
-  	if((j-1)%3==0&&j-1>0) printf(",");
+  	printf("%d",d[i-1]);
+  	if((i-1)%3==0&&i-1>0) printf(",");
   }
+  
 	return 0;
  } 
